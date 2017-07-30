305335cfa0f77668dfc153da4f095b8387261df1
strdr4605 <str.dr4605@gmail.com>
2017-07-30 16:12:31 +0300

    close #5 Add StarsLayer

--- Roop/src/app.js
+++ Roop/src/app.js
@@ -10,6 +10,12 @@ var GameLayer = cc.Layer.extend({
         this.spaceshipSprite.setPosition(cc.p(size.width / 2, size.height / 2));
         this.spaceshipSprite.setScale(0.3);
         this.addChild(this.spaceshipSprite, kZindexSpaceship);
+        this.startStarLayer = {
+            x: 0,
+            y: 0
+        };
+        this.starLayer = new StarsLayer(this.startStarLayer, 10, 10);
+        this.addChild(this.starLayer);
         // var animationSprite = new AnimationSprite("meteors_128x128_8x8.png", new cc.Size(128,128), new cc.Size(8,8));
         // this.sprite.runAction(animationSprite.actions[0]);
     },
